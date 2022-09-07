87f938e060291bd4c102f1e00a6fe62a76723188
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-07 15:56:48 -0500

    adding material UI components to make the menu for big screen: NavBar.js

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,7 +1,36 @@
-import { AppBar, Toolbar } from '@material-ui/core'
+import { AppBar, Toolbar, List } from '@material-ui/core'
 import React from 'react'
 import logo from "../../img/logoH.png"
 import { makeStyles } from '@material-ui/core'
+import {Link, animateScroll as scroll} from "react-scroll"
+import {InfoTwoToneIcon} from "@material-ui/icons/InfoTwoTone"
+import {EmojiObjectsTwoToneIcon} from "@material-ui/icons/EmojiObjectsTwoTone";
+import {BuildTwoToneIcon} from "@material-ui/icons/BuildTwoTone";
+import {ContactMailTwoToneIcon} from "@material-ui/icons/ContactMailTwoTone";
+
+const links =[
+  {
+    id: "about",
+    text: "About me",
+    icon: <InfoTwoToneIcon />
+  },
+  {
+    id: "skills",
+    text: "My Padawan Journey",
+    icon: <EmojiObjectsTwoToneIcon />
+  },
+  {
+    id: "work",
+    text: "My Work",
+    icon: <BuildTwoToneIcon />
+  },
+  {
+    id: "contact",
+    text: "Get in touch",
+    icon: <ContactMailTwoToneIcon />
+  },
+
+]
 
 const NavBar = () => {
   const classes= useStyles()
@@ -9,6 +38,13 @@ const NavBar = () => {
     <AppBar position="sticky" className={classes.root}>
            <Toolbar className={classes.toolbar}>
            <img src={logo} alt="Programmer" className={classes.logo} /> 
+           <List className={classes.menu}>
+            {
+              links.map(({id, text}, index) =>(
+                <Link key={index} to={id} spy={true} smooth={true} duration={500} offset={-70}>{text}</Link>
+              ))
+            }
+           </List>
             </Toolbar>         
      </AppBar>
 
@@ -19,6 +55,10 @@ const NavBar = () => {
 const useStyles= makeStyles((theme) => ({
   root:{
     backgroundColor: "#E6E6E6",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 999
   },
   logo: {
    width: "300px",
