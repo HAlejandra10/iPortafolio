7174ff94496740acba62072cd06dc336c6c67220
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-11 18:27:25 -0500

    bringing the data from mockData into the MyWork file with .map, to indicate the size references toth difference devices, add a Link componente from Material UI and put in href and attribute 'rel=noopener noreferrer' tis attribute is to indicate to web navigator to be safe the place and no allow  intruder

--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -1,5 +1,8 @@
-import { makeStyles, Typography } from '@material-ui/core'
+import { Card, CardContent, CardMedia, Grid, Link, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+import mockData from '../../mockData'
+
+
 
 const MyWork = ({title, id, dark}) => {
   const classes = useStyles()
@@ -7,6 +10,22 @@ const MyWork = ({title, id, dark}) => {
     <div className={`${classes.section} ${dark && classes.sectiondark}`}>
       <div className={classes.sectioncontent} id={id}>
         <Typography variant="h3">{title}</Typography>
+        <Grid container className={classes.grid}>
+          {
+            mockData.map(({title, imageSrc, link},index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia />
+                  <CardContent>
+                    <Link href={link} color="primary" target="_blank" rel="noopener noreferrer">
+                      
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
       </div>
     </div>
   )
---
+++ src/mockData.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import imagen tomada para el imageScr from "../../img/imagenNombre.png"
+import mokepon from "./img/mokepon.png"
+
+const mockData = [
+    {
+        title: "Mokepon Game",
+        imageSrc: mokepon,
+        link: "https://github.com/HAlejandra10/MOKEPON",
+    },
+    {   
+        title: "nombre del proyecto",
+        imageSrc: "imagen o screenshot del proyecto",
+        link: "link de la demo del proyecto",
+    },
+    {
+
+    }
+] 
+
+export default mockData
