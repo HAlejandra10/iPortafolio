65e6dc624dff89f9bae0ffdd78f6223c116f6ef0
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-11 17:43:42 -0500

    add a new component(StarRating), and adding into the Technologies file a TimelineContent of Material UI to keep building stars section to show the stars into the cards and to add styles

--- src/components/layout/Technologies.js
+++ src/components/layout/Technologies.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import html from "../../img/html.png"
-import css from "../../img/html.png"
-import javascript from "../../img/JavaScript_Logo.png"
-import reactjs from "../../img/React_Logo.png"
-import git from "../../img/Git_Logo.png"
-import Timeline from "@material-ui/lab/Timeline" 
-import TimelineItem from "@material-ui/lab/TimelineItem"
-import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent"
+import Timeline from "@material-ui/lab/Timeline" ;
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import { makeStyles, Typography } from '@material-ui/core';
-import TimelineSeparator from "@material-ui/lab/TimelineSeparator"
-import TimelineConnector from "@material-ui/lab/TimelineConnector"
-import TimelineDot from "@material-ui/lab/TimelineDot"
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import Paper from "@material-ui/core/Paper/Paper";
+import pic1 from "../../img/html.png";
+import pic2 from "../../img/css.png";
+import pic3 from "../../img/JavaScript_Logo.png"
+import pic4 from "../../img/React_Logo.png"
+import pic5 from "../../img/Git_Logo.png"
+import StarRating from './StarRating';
+
 
 
 
@@ -21,57 +25,59 @@ const Technologies = () => {
     const skills =[
         {
             year: "2021",
-            src: { html },
-            title: "HTML 5",
-            stars: 4
+            image: pic1,
+            title: "HTML5",
+            stars: 4,
         },
         {
             year: "2021",
-            // src: {css},
+            image: pic2,
             title: "CSS",
-            stars: 4
+            stars: 4,
         },
         {
             year:"2021",
-            // src: {javascript},
+            image: pic3,
             title: "JavaScript",
-            stars: 4
+            stars: 4,
         },
         {
             year:"2021",
-            // src: {reactjs},
+            image: pic4,
             title: "ReactJs",
-            stars: 4
+            stars: 4,
         },
         {
             year:"2021",
-            // src: {git},
+            image: pic5,
             title: "Git",
-            stars: 4
+            stars: 4,
         },
     ]
 
   return (
-
     <Timeline align="left">
-
-        Technologies
-         {skills.map(({year, src, title, starts}, index) => (
+         {skills.map(({year, image, title, stars}, index) => (
             <TimelineItem key={index}>
                 <TimelineOppositeContent>
                     <Typography variant="h6" color="textSecondary">
                         {year}
-                        
                     </Typography>
                 </TimelineOppositeContent>
                 <TimelineSeparator>
                     <TimelineDot>
-                    {/* <img src={src} alt={title} className={classes.cusstomlogo}/> */}
-                    <img src={src} alt={title} className={classes.cusstomlogo}/>
+                    <img src={image} alt={title} className={classes.cusstomlogo} />
                     </TimelineDot>
                     <TimelineConnector/>
                 </TimelineSeparator>
-                
+                <TimelineContent>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Typography variant="h6" component="h1">
+                        {title}
+                        </Typography>
+                        <StarRating stars={stars}/>
+                    </Paper>
+                </TimelineContent>
             </TimelineItem>
         ))
         }
@@ -82,9 +88,11 @@ const Technologies = () => {
 const useStyles= makeStyles((theme) => ({
     cusstomlogo: {
         width: "25px",
-      
-  
     },
+    paper: {
+        padding: "6px 16px",
+        maxWidth: "200px"
+    }
 }))
 
 export default Technologies
