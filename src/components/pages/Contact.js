8bf83c0de946dfb326d24efe2564559460d7dd1e
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-12 10:23:37 -0500

    add Typography to the title and after to adda div to wrap the buttons and add the form tag to put into the the field to the name and email, after a logic to change to the section of form, it means, changing from Say Hi to Get A Quote and the last, add styles(Contact.js)

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,52 @@
-import React from 'react'
-import { makeStyles, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { makeStyles, Paper, Typography, Radio, TextField, Button } from '@material-ui/core'
 
 const Contact = ({title, id, dark}) => {
   const classes = useStyles()
+  const [value, setValue]= useState("Say Hi")
+
+  const handleChange= (e) => {
+    setValue(e.target.value)
+  }
   return (
     <div className={`${classes.section} ${dark && classes.sectiondark}`}>
       <div className={classes.sectioncontent} id={id}>
         <Typography variant="h3">{title}</Typography>
+        <Paper className={classes.root}>
+          <div className={classes.titleandchoices}>
+          <Typography variant="h5">CONTACT ME</Typography>
+          <div className={classes.choices}>
+            <span>Say Hello</span>
+            <Radio 
+            value= "Say Hi"
+            checked={ value == "Say Hi"}
+            color="primary"
+            onChange={handleChange}
+            />
+              <span>Get A Quote</span>
+            <Radio 
+            value= "Get A Quote"
+            checked={ value == "Get A Quote"}
+            color="primary"
+            onChange={handleChange}
+            />
+          </div>
+          </div>
+          <form className={classes.form} validate autoComplete="off">
+            <TextField label="Your Name"/>
+            <TextField label="Your e-mail"/>
+            {
+               value === "Get A Quote" ? (
+                <>
+                 <TextField label="Needed Services"/>
+                 <TextField label="Stimaded budget"/>
+                </>
+               ) : null
+            }
+            <TextField label="Write a message"/>
+          </form>
+          <Button variant="contained">Submit</Button>
+        </Paper>
       </div>
     </div>
   )
@@ -14,15 +54,19 @@ const Contact = ({title, id, dark}) => {
 
 const useStyles= makeStyles((theme) => ({
   section: {
-    minHeight: "100vh"
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   sectiondark:{
     background: "#333",
     color: "#fff"
   },
   sectioncontent: {
-    maxWidth: "80vw", 
-    margin: "0 auto"
+   width: "80vw",
+  //  border: "2px solid red",
   }
 }))
 
