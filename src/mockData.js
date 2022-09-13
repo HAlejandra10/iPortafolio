438bcbde00e680873e478cea69607f959b208a55
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-13 18:03:46 -0500

    Deployed webs porta

--- src/mockData.js
+++ src/mockData.js
@@ -5,7 +5,7 @@ const mockData = [
     {
         title: "Portafolio",
         image: porta,
-        link: "https://boisterous-dango-fac437.netlify.app/",
+        link: "https://github.com/HAlejandra10/MOKEPON",
     },
     {   
         title: "Next project",
