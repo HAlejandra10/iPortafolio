a4ac6bacb61a7e7cd8e6fd9ed8c303962127e4e7
HeidyAPrimo <heidyprimo10@outlook.com>
2022-09-13 18:39:38 -0500

    Updates

---
+++ precache-manifest.c40e981b88425e3e08179fe299f5cdcb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b864c0e7618159d692b1",
+    "url": "/iPortafolio/static/css/main.41916cb3.chunk.css"
+  },
+  {
+    "revision": "b864c0e7618159d692b1",
+    "url": "/iPortafolio/static/js/main.b864c0e7.chunk.js"
+  },
+  {
+    "revision": "7ca6c05596d8372f48fd",
+    "url": "/iPortafolio/static/js/1.7ca6c055.chunk.js"
+  },
+  {
+    "revision": "adb65671f33921fc30c3",
+    "url": "/iPortafolio/static/js/runtime~main.adb65671.js"
+  },
+  {
+    "revision": "333a01d020ecae36e78317283e052203",
+    "url": "/iPortafolio/static/media/ifoto.333a01d0.png"
+  },
+  {
+    "revision": "4603894506a17deeb3f1769da482ca98",
+    "url": "/iPortafolio/static/media/Heidy_Primo_CV.46038945.pdf"
+  },
+  {
+    "revision": "c573ff5552d6da9a1d28ec4e27cd1445",
+    "url": "/iPortafolio/static/media/html.c573ff55.png"
+  },
+  {
+    "revision": "e493a5e625415e4cbaf6fa9129b74ba6",
+    "url": "/iPortafolio/static/media/css.e493a5e6.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/iPortafolio/static/media/JavaScript_Logo.0cded3a3.png"
+  },
+  {
+    "revision": "360a0ef4666dcbbd62d979e7100b4a6d",
+    "url": "/iPortafolio/static/media/React_Logo.360a0ef4.png"
+  },
+  {
+    "revision": "1c8f8077bee2d80b5ce3191d7fed1a81",
+    "url": "/iPortafolio/static/media/porta.1c8f8077.png"
+  },
+  {
+    "revision": "1f133fd58badce4e13cf752efe647b32",
+    "url": "/iPortafolio/static/media/logo.1f133fd5.gif"
+  },
+  {
+    "revision": "af3c642e473d7eaadea12679210cf3d3",
+    "url": "/iPortafolio/index.html"
+  }
+];
